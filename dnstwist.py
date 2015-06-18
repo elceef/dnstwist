219aeb8b9fcb8c238a92501200eb425f64dc813b
Marcin Ulikowski <elceef@gmail.com>
2015-06-18 10:52:58 +0200

    Improved homoglyph generator

--- dnstwist.py
+++ dnstwist.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 #
 # dnstwist by marcin@ulikowski.pl
-# Generate and resolve domain variations to detect typo squatting, phishing and corporate espionage.
+# Generate and resolve domain variations to detect typo squatting,
+# phishing and corporate espionage.
 #
 #
 # dnstwist is free software; you can redistribute it and/or modify
@@ -18,7 +19,7 @@
 # along with dnstwist.  If not, see <http://www.gnu.org/licenses/>.
 
 __author__ = 'Marcin Ulikowski'
-__version__ = '20150617'
+__version__ = '20150618'
 __email__ = 'marcin@ulikowski.pl'
 
 import sys
@@ -46,6 +47,7 @@ def bitsquatting(domain):
 	dom = domain.rsplit('.', 1)[0]
 	tld = domain.rsplit('.', 1)[1]
 	masks = [1, 2, 4, 8, 16, 32, 64, 128]
+
 	for i in range(0, len(dom)):
 		c = dom[i]
 		for j in range(0, len(masks)):
@@ -53,43 +55,53 @@ def bitsquatting(domain):
 			o = ord(b)
 			if (o >= 48 and o <= 57) or (o >= 97 and o <= 122):
 				out.append(dom[:i] + b + dom[i+1:] + '.' + tld)
+
 	return out
 
 def homoglyph(domain):
-	glyphs = { 'd':['b', 'cl'], 'm':['n', 'rn'], 'l':['1', 'i'], 'o':['0'], 'w':['vv'], 'n':['m'], 'b':['d'], 'i':['l'] }
+	glyphs = { 'd':['b', 'cl'], 'm':['n', 'rn'], 'l':['1', 'i'], 'o':['0'], 'w':['vv'], 'n':['m'], 'b':['d'], 'i':['l'], 'g':['q'], 'q':['g'] }
 	out = []
 	dom = domain.rsplit('.', 1)[0]
 	tld = domain.rsplit('.', 1)[1]
+
 	for ws in range(0, len(dom)):
-		for i in range(0, len(dom)-ws):
+		for i in range(0, (len(dom)-ws)+1):
 			win = dom[i:i+ws]
 			j = 0
+
 			while j < ws:
 				c = win[j]
 				if c in glyphs:
 					for g in range(0, len(glyphs[c])):
 						win = win[:j] + glyphs[c][g] + win[j+1:]
+
 						if len(glyphs[c][g]) > 1:
 							j += len(glyphs[c][g]) - 1
 						out.append(dom[:i] + win + dom[i+ws:] + '.' + tld)
+
 				j += 1
+
 	return list(set(out))
 
 def repetition(domain):
 	out = []
 	dom = domain.rsplit('.', 1)[0]
 	tld = domain.rsplit('.', 1)[1]
+
 	for i in range(0, len(dom)):
 		if dom[i].isalpha():
 			out.append(dom[:i] + dom[i] + dom[i] + dom[i+1:] + '.' + tld)
+
 	return out
 
 def transposition(domain):
 	out = []
 	dom = domain.rsplit('.', 1)[0]
 	tld = domain.rsplit('.', 1)[1]
+
 	for i in range(0, len(dom)-1):
 		out.append(dom[:i] + dom[i+1] + dom[i] + dom[i+2:] + '.' + tld)
+
 	return out
 
 def replacement(domain):
@@ -102,10 +114,12 @@ def replacement(domain):
 	out = []
 	dom = domain.rsplit('.', 1)[0]
 	tld = domain.rsplit('.', 1)[1]
+
 	for i in range(0, len(dom)):
 		if dom[i] in keys:
 			for c in range(0, len(keys[dom[i]])):
 				out.append(dom[:i] + keys[dom[i]][c] + dom[i+1:] + '.' + tld)
+
 	return out
 
 
@@ -113,8 +127,10 @@ def omission(domain):
 	out = []
 	dom = domain.rsplit('.', 1)[0]
 	tld = domain.rsplit('.', 1)[1]
+
 	for i in range(0, len(dom)):
 		out.append(dom[:i] + dom[i+1:] + '.' + tld)
+
 	return out
 
 def insertion(domain):
@@ -133,6 +149,7 @@ def insertion(domain):
 			for c in range(0, len(keys[dom[i]])):
 				out.append(dom[:i] + keys[dom[i]][c] + dom[i] + dom[i+1:] + '.' + tld)
 				out.append(dom[:i] + dom[i] + keys[dom[i]][c] + dom[i+1:] + '.' + tld)
+
 	return out
 
 def fuzz_domain(domain):
