17bf31cd03bc01bd4f1b1866fd74cf9384417a9a
Marcin Ulikowski <elceef@gmail.com>
2015-09-16 14:53:54 -0400

    Switched from GPL to Apache License

--- dnstwist.py
+++ dnstwist.py
@@ -1,22 +1,21 @@
 #!/usr/bin/env python
 #
-# dnstwist by marcin@ulikowski.pl
+# dnstwist
+#
 # Generate and resolve domain variations to detect typo squatting,
 # phishing and corporate espionage.
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
-# dnstwist is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# dnstwist is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with dnstwist.  If not, see <http://www.gnu.org/licenses/>.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 __author__ = 'Marcin Ulikowski'
 __version__ = '20150910'
