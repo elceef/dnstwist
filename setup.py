977cfecbf606eec02783f2174a2c0b32dea7dfd0
Marcin Ulikowski <elceef@gmail.com>
2020-04-28 18:37:37 +0200

    A few tweaks in setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 import setuptools
+from dnstwist import __version__ as version
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
 
 setuptools.setup(
-    name='dnstwist',
-    version='1.02',
-    author="Marcin Ulikowski",
-    author_email="marcin@ulikowski.pl",
-    description="See what sort of trouble users can get in trying to type your domain name. Find similar-looking domains that adversaries can use to attack you.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/elceef/dnstwist",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
+	name='dnstwist',
+	version=version,
+	author='Marcin Ulikowski',
+	author_email='marcin@ulikowski.pl',
+	description='Domain name permutation engine for detecting homograph phishing attacks, typo squatting, and brand impersonation.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	url='https://github.com/elceef/dnstwist',
+	packages=setuptools.find_packages(),
+	entry_points={
+		'console_scripts': ['dnstwist=dnstwist:main']
+	},
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: Apache Software License',
+		'Operating System :: OS Independent',
+	],
 )
