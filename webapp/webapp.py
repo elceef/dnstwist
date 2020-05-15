79a5304d7bf8dda222208d713fb52d80ba78ce72
Marcin Ulikowski <elceef@gmail.com>
2020-05-15 19:59:36 +0200

    Implemented parallel scans

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from os import environ
+import os
 from queue import Queue
+from uuid import uuid4
+from time import time
 
 from flask import Flask, request, jsonify, send_from_directory
 
 from dnstwist import UrlParser, DomainFuzz, DomainThread, THREAD_COUNT_DEFAULT
 
 
-API_PORT = int(environ.get('HTTP_PORT', 8000))
-API_HOST= environ.get('API_HOST', '127.0.0.1')
-THREAD_COUNT = int(environ.get('THREADS', THREAD_COUNT_DEFAULT))
-NAMESERVER = environ.get('NAMESERVER')
+PORT = int(os.environ.get('PORT', 8000))
+HOST= os.environ.get('HOST', '127.0.0.1')
+THREAD_COUNT = int(os.environ.get('THREAD_COUNT', THREAD_COUNT_DEFAULT))
+NAMESERVER = os.environ.get('NAMESERVER')
+SESSION_TTL = int(os.environ.get('SESSION_TTL', 300))
+SESSION_MAX = int(os.environ.get('SESSION_MAX', 20))
 WEBAPP_HTML = 'webapp.html'
+WEBAPP_DIR = os.path.dirname(__file__)
 
 
-session = None
+sessions = []
 app = Flask(__name__)
 
 
 class Session():
 	def __init__(self, url, nameserver=None, thread_count=THREAD_COUNT):
+		self.id = str(uuid4())
+		self.timestamp = int(time())
 		self.url = UrlParser(url)
 		self.nameserver = nameserver
 		self.thread_count = thread_count
@@ -48,12 +55,15 @@ class Session():
 		self.jobs.queue.clear()
 		for worker in self.threads:
 			worker.stop()
+			worker.join()
 
 	def status(self):
 		total = len(self.permutations)
 		remaining = self.jobs.qsize()
 		complete = total - remaining
 		return {
+			'id': self.id,
+			'timestamp': self.timestamp,
 			'url': self.url.full_uri(),
 			'total': total,
 			'complete': complete,
@@ -66,44 +76,61 @@ class Session():
 
 @app.route('/')
 def root():
-	return send_from_directory('.', WEBAPP_HTML)
+	return send_from_directory(WEBAPP_DIR, WEBAPP_HTML)
 
 
-@app.route('/api/scan', methods=['POST'])
+@app.route('/api/scans', methods=['POST'])
 def api_scan():
-	global session
-	if session:
-		if session.status().get('remaining', 0) > 0:
-			return jsonify({'message': 'Another scan session is running'}), 500
+	for s in sessions:
+		status = s.status()
+		if status['remaining'] == 0 and (status['timestamp'] + SESSION_TTL) < time():
+			sessions.remove(s)
+	if len(sessions) >= SESSION_MAX:
+		return jsonify({'message': 'Too many scan sessions - please retry in a minute'}), 500
 	if 'url' not in request.json:
 		return jsonify({'message': 'Invalid request'}), 400
-
+	for suburl in request.json['url'].split('.'):
+		if len(suburl) > 15:
+			return jsonify({'message': 'Domain name is too long'}), 400
 	try:
 		session = Session(request.json.get('url'), nameserver=NAMESERVER, thread_count=THREAD_COUNT)
 	except Exception as err:
 		return jsonify({'message': 'Invalid domain name'}), 400
-
-	session.scan()
-
-	return jsonify({})
-
-
-@app.route('/api/status')
-def api_status():
-	return jsonify(session.status() if session else {})
+	else:
+		session.scan()
+		sessions.append(session)
+	return jsonify(session.status()), 201
 
 
-@app.route('/api/domains')
-def api_domains():
-	return jsonify(session.domains() if session else [])
+@app.route('/api/scans/<sid>')
+def api_status(sid):
+	for s in sessions:
+		if s.id == sid:
+			return jsonify(s.status())
+	return jsonify({'message': 'Scan session not found'}), 404
 
 
-@app.route('/api/stop', methods=['POST'])
-def api_stop():
-	if session:
-		session.stop()
-	return jsonify({})
+@app.route('/api/scans/<sid>/domains')
+def api_domains(sid):
+	for s in sessions:
+		if s.id == sid:
+			return jsonify(s.domains())
+	return jsonify({'message': 'Scan session not found'}), 404
+
+
+@app.route('/api/scans/<sid>/stop', methods=['POST'])
+def api_stop(sid):
+	for s in sessions:
+		if s.id == sid:
+			s.stop()
+			return jsonify({})
+	return jsonify({'message': 'Scan session not found'}), 404
+
+
+@app.route('/api/healthcheck')
+def api_healthcheck():
+	return jsonify({'message': 'OK'})
 
 
 if __name__ == '__main__':
-	app.run(host=API_HOST, port=API_PORT)
+	app.run(host=HOST, port=PORT)
